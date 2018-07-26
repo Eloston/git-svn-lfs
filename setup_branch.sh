3ed373399f28daa8c9778627eb4fa09477357077
Eloston <eloston@programmer.net>
2018-07-26 07:00:18 +0000

    Update git-svn's git index file for each branch

--- setup_branch.sh
+++ setup_branch.sh
@@ -15,3 +15,7 @@ git reflog expire --expire-unreachable=now --all
 git gc --prune=now
 git branch -D master || true
 $(dirname $(readlink -f $0))/update_rev_map.py $1 "$(git show-ref -s refs/heads/$1)"
+git diff --exit-code # Make sure working tree is clean, otherwise LFS could be broken
+git diff --cached --exit-code # Make sure index is good, otherwise we would be copying a bad index into git-svn
+mv .git/svn/refs/remotes/svn/$1/index .git/svn/refs/remotes/svn/$1/index_backup
+cp .git/index .git/svn/refs/remotes/svn/$1/index
